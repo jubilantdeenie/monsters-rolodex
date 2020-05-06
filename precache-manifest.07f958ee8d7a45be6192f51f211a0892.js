9bd6ebe0b37d6210667b7df9de9af70b3e9880e5
Deenie Wallace <deenie.wallace@u.group>
2020-05-06 13:06:42 -0700

    Updates

---
+++ precache-manifest.07f958ee8d7a45be6192f51f211a0892.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b84f8b0cbf5351051b740ebc9de0905c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2a8909a81a5dbcd0ef81",
+    "url": "/monsters-rolodex/static/css/main.41ff4ee6.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a8909a81a5dbcd0ef81",
+    "url": "/monsters-rolodex/static/js/main.d6af63eb.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
